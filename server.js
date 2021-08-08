b79668b20631aa7c64009147ebe551a6aff957c0
Anthony Pena <apena5@alumni.jh.edu>
2021-08-07 21:02:05 -0600

    connected to database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Antpen97!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 app.use((req, res) => {
     res.status(404).end();
